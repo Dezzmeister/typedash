09b5e67f7a8f1bba0b135ad4a307fd30cf516bb4
Joe Desmond <joseph.desmond@grabcad.com>
2024-01-04 11:07:12 -0500

    Implemented subtraction of natural numbers with support for negative results

--- src/math.ts
+++ src/math.ts
@@ -55,6 +55,7 @@ type DigitSumMap = {
     '88': [1, 6];   '98': [1, 7];
     '89': [1, 7];   '99': [1, 8];
 };
+type DigitNinesComplement = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
 // Returns the sum of two base-10 digits, and the carry. The shape of the result is
 // [Sum extends Digit, Carry extends 0 | 1]
@@ -68,6 +69,18 @@ type DigitSumWithCarry<T extends Digit, U extends Digit, Carry extends 1 | 0> =
             [NextDigit[C], 0] :
             [C, NextDigit[V]]
     : never;
+
+type NinesComplement<T extends string> = 
+    T extends `${infer Head extends Digit}${infer Tail}` ?
+        `${DigitNinesComplement[Head]}${NinesComplement<Tail>}` :
+    '';
+
+// Converts a positive number (as a string) to a negative, but not
+// vice-versa
+type ToNegative<T extends string> = 
+    `-${T}` extends `${infer N extends number}` ?
+        N :
+    never;
 
 /**
  * Pads the shorter of two strings with trailing zeroes until the strings are the same
@@ -211,6 +224,25 @@ export type Sum<T extends number, U extends number> =
         SumFloatsImpl<TWholeRaw, TFracRaw, UWholeRaw, UFracRaw> :
     never;
 
+/**
+ * Computes the difference between two natural numbers. The result will be negative if T is greater than U.
+ * This is implemented by taking the nine's complement of U and adding it to T, much like binary subtraction.
+ */
+export type DiffNats<T extends number, U extends number> = 
+    WithTrailingZeroes<Reversed<`${T}`>, Reversed<`${U}`>> extends [infer Minuend extends string, infer Subtrahend extends string] ?
+        SumImplWithOverflow<NinesComplement<Minuend>, Subtrahend> extends [infer RawSum extends string, infer Carry extends 0 | 1] ?
+            // The result is positive
+            Carry extends 0 ?
+                WithoutLeadingZeroes<NinesComplement<Reversed<RawSum>>> extends `${infer Res extends number}` ?
+                    Res :
+                never :
+            // The result is negative
+            WithTrailingZeroes<RawSum, '1'> extends [infer RawSum2R extends string, infer Carry2 extends string] ?
+                ToNegative<WithoutLeadingZeroes<Reversed<SumImpl<RawSum2R, Carry2>>>> :
+            never :
+        never :
+    never;
+
 // @ts-ignore (unused)
 type _test = [
     Expect<Eq<SumNats<0, 0>, 0>>,
@@ -227,6 +259,37 @@ type _test = [
     Expect<Eq<SumNats<1001, 110>, 1111>>,
     Expect<Eq<SumNats<1001, 1000>, 2001>>,
     Expect<Eq<SumNats<1001, 999>, 2000>>,
+
+    Expect<Eq<DiffNats<0, 0>, 0>>,
+    Expect<Eq<DiffNats<1, 0>, 1>>,
+    Expect<Eq<DiffNats<0, 1>, -1>>,
+    Expect<Eq<DiffNats<1, 1>, 0>>,
+    Expect<Eq<DiffNats<9, 5>, 4>>,
+    Expect<Eq<DiffNats<9, 4>, 5>>,
+    Expect<Eq<DiffNats<4, 9>, -5>>,
+    Expect<Eq<DiffNats<5, 9>, -4>>,
+    Expect<Eq<DiffNats<1_000_000_000, 0>, 1_000_000_000>>,
+    Expect<Eq<DiffNats<1_000_000_000, 1_000_000_000>, 0>>,
+    Expect<Eq<DiffNats<1_000_000_000, 999_999_999>, 1>>,
+    Expect<Eq<DiffNats<1_000_000_000, 1>, 999_999_999>>,
+    Expect<Eq<DiffNats<999_999_999, 1_000_000_000>, -1>>,
+    Expect<Eq<DiffNats<1, 1_000_000_000>, -999_999_999>>,
+    Expect<Eq<DiffNats<109, 99>, 10>>,
+    Expect<Eq<DiffNats<109, 10>, 99>>,
+    Expect<Eq<DiffNats<10, 109>, -99>>,
+    Expect<Eq<DiffNats<99, 109>, -10>>,
+    Expect<Eq<DiffNats<1111, 1001>, 110>>,
+    Expect<Eq<DiffNats<1111, 110>, 1001>>,
+    Expect<Eq<DiffNats<1001, 1111>, -110>>,
+    Expect<Eq<DiffNats<110, 1111>, -1001>>,
+    Expect<Eq<DiffNats<2001, 1001>, 1000>>,
+    Expect<Eq<DiffNats<2001, 1000>, 1001>>,
+    Expect<Eq<DiffNats<1001, 2001>, -1000>>,
+    Expect<Eq<DiffNats<1000, 2001>, -1001>>,
+    Expect<Eq<DiffNats<2000, 1001>, 999>>,
+    Expect<Eq<DiffNats<2000, 999>, 1001>>,
+    Expect<Eq<DiffNats<1001, 2000>, -999>>,
+    Expect<Eq<DiffNats<999, 2000>, -1001>>,
 
     // Ensure that SumFloats handles SumNats cases properly
     Expect<Eq<SumFloats<0, 0>, 0>>,
